9963aef70e00848f2ef03be80396edcfa45b4435
trence <896063260@qq.com>
2018-09-22 14:24:22 +0800

    fix

--- start.sh
+++ start.sh
@@ -26,6 +26,6 @@ docker run --rm -v $PWD:/gitbook -p 4000:4000 billryan/gitbook gitbook build
 
 #rm build -rf
 
-#mv _book build && chmod 777 build
+mv _book /gitbook && chmod 777 build
 
-docker run --name my-nginx -v  $PWD/_build:/usr/share/nginx/html -d -p 80:80 nginx
+docker run --name my-nginx -v  /gitbook:/usr/share/nginx/html -d -p 80:80 nginx
