2381de4373939203c142bac2aee0db89731cd587
trence <896063260@qq.com>
2018-09-22 14:45:59 +0800

    fix

--- start.sh
+++ start.sh
@@ -16,18 +16,14 @@ docker rm  -f my-nginx &> /dev/null
 #docker pull billryan/gitbook #https://hub.docker.com/r/billryan/gitbook/
 
 #docker pull nginx
-docker run --rm -v $PWD:/gitbook -p 4000:4000 billryan/gitbook gitbook init
+#docker run --rm -v $PWD:/gitbook -p 4000:4000 billryan/gitbook gitbook init
 
-docker run --rm -v $PWD:/gitbook -p 4000:4000 billryan/gitbook gitbook install
+#docker run --rm -v $PWD:/gitbook -p 4000:4000 billryan/gitbook gitbook install
 
-docker run --rm -v $PWD:/gitbook -p 4000:4000 billryan/gitbook gitbook build
+#docker run --rm -v $PWD:/gitbook -p 4000:4000 billryan/gitbook gitbook build
 
-ls -al
+#ls -al
 
-rm build -rf
-
-mv _book build && chmod 777 build -R
-
-ls -al
+#mv _book build && chmod 777 build -R
 
 docker run --name my-nginx -v  $PWD/build:/usr/share/nginx/html -d -p 80:80 nginx
